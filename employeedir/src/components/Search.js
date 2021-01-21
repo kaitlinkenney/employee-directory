0c9ec5fa253299f57842571b019d0955a6e28103
kaitlinkenney <kaitlin.kenney96@gmail.com>
2021-01-20 20:00:43 -0500

    names sorting

--- employeedir/src/components/Search.js
+++ employeedir/src/components/Search.js
@@ -6,20 +6,15 @@ class Search extends Component {
     state = {
         search: ''
     }
-   
-    // const searched = props.result.filter(phonenum => phonenum.phone);
+
 
     onChange = (x) => this.setState({ search: x.target.value})
-
-
-
-    
     
     
     render() {
         return (
             <form>
-                <input type="text" name="search" placeholder="Search" onChange={this.onChange}/>
+                <input type="text" name="search" placeholder="Search" onChange={this.getValueInput}/>
             </form>
 
         )
--- employeedir/src/components/TableContainer.js
+++ employeedir/src/components/TableContainer.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import Rows from "./Rows";
 import API from "../utils/Api";
+import Search from "./Search";
 
 class TableContainer extends Component {
 
   state = {
-    result: ""
+    result: "",
+    search: ""
   }
 
 
@@ -13,58 +15,84 @@ class TableContainer extends Component {
     API.getPeopleList()
       .then(res => this.setState({ result: res.data }))
       // .then(res=> console.log(res.data))
-    
-      .catch(err => console.log(err));
-      console.log(this.state.result)
-  }
 
-  componentDidUpdate(){
+      .catch(err => console.log(err));
     console.log(this.state.result)
   }
-  // componentDidMount(){
-  // console.log(this.state.result)
-  // }
 
-  // sortNames = (x) => {
-  //   this.result.results.name.first.sort()
-  // }
+  componentDidUpdate() {
+    console.log(this.state.result)
+  }
+
+  getValueInput(evt) {
+    const inputValue = evt.target.value;
+    console.log(inputValue)
+    this.filterNames(inputValue);
+  }
+
+  filterNames(inputValue) {
+    const { result } = this.state;
+    this.setState({
+      filtered: result.filter(item =>
+        item.results.phone.includes(inputValue)),
+    });
+  }
+
+  sortNames = () => {
+
+    const newResults = this.state.result.results.sort(function (a, b) {
+
+      console.log(b.name.first)
+      console.log(a.name.first)
+
+      const sortedNamesA = a.name.first
+      const sortedNamesB = b.name.first
+      if (
+        sortedNamesA < sortedNamesB
+      ) {
+        return -1
+      }
+      if (
+        sortedNamesB < sortedNamesA) {
+        return 1
+      }
+      return 0
+    })
+    this.setState({ newResults })
+  }
+
+  onChange = (x) => this.setState({ search: x.target.value})
 
   render() {
     return (
-      <table className="table">
-        <thead>
-          <tr>
-            <th scope="col">Image</th>
-            <th scope="col" onClick={this.sortNames}>Name</th>
-            <th scope="col">Phone</th>
-            <th scope="col">Email</th>
-            <th scope="col">DOB</th>
-          </tr>
-        </thead>
-        <tbody>
-          {/* {this.state.result==="cat" ? console.log("no result") : console.log(this.state.result)} */}
-          {/* {this.state.result.map(cool => (
-            < Rows
-              image={cool.results.picture.thumbnail}
-              key={cool.results.phone}
-              name={cool.results.name.first}
-              phone={cool.results.phone}
-              email={cool.results.email}
-              dob={cool.results.dob.date}
-            />
-          ))} */}
-             {this.state.result==="" ? console.log("no result") : (this.state.result.results.map(cool => (
-            < Rows
-              image={cool.picture.thumbnail}
-              key={cool.phone}
-              name={cool.name.first}
-              phone={cool.phone}
-              email={cool.email}
-              dob={cool.dob.date}
-            />
-          )))}
-        </tbody>
-      </table>
+      <div>
+        <form>
+          <input type="text" name="search" placeholder="Search" onChange={this.getValueInput} />
+        </form>
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">Image</th>
+              <th scope="col" onClick={this.sortNames}>Name</th>
+              <th scope="col">Phone</th>
+              <th scope="col">Email</th>
+              <th scope="col">DOB</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.result === "" ? console.log("no result") : (this.state.result.results.map(cool => (
+              < Rows
+                image={cool.picture.thumbnail}
+                key={cool.phone}
+                name={cool.name.first}
+                phone={cool.phone}
+                email={cool.email}
+                dob={cool.dob.date}
+              />
+            )))}
+          </tbody>
+        </table>
+      </div>
     )
   }
 
@@ -76,15 +104,3 @@ export default TableContainer;
 
 
 
-   // getValueInput (evt) {
-    //     const inputValue = evt.target.value;
-    //     this.filterNames(inputValue);
-    //   }
-
-    // filterNames (inputValue) {
-    //   const { result } = this.state;
-    //   this.setState({
-    //     filtered: result.filter(item => 
-    //        item.rsults.phone.includes(inputValue)),
-    //   });
-    // }
